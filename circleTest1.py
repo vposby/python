971a158e832ebfb6ee230de8328a03a0799d71b1
Tori Osby <vposby@gmail.com>
2019-05-01 17:52:57 -0700

    Update circleTest1.py

--- circleTest1.py
+++ circleTest1.py
@@ -8,7 +8,10 @@ sizes randomly.
 import sys, pygame, random
 pygame.init()
 
-size = width, height = (512, 512)
+#display size
+size = width, height = (512, 512) 
+
+#colors
 black = (0,0,0)
 white = (255,255,255)
 red = (255,0,0)
@@ -18,17 +21,21 @@ yellow = (255,255,0)
 pink = (255,0,255)
 cyan = (0,255,255)
 
+#circle positions
 topRt = (int(width/4),int(height/3))
 topMid = (int(width/2),int(height/3))
 topLt = (int(3*width/4),int(height/3))
 btmRt = (int(width/4),int(2*height/3))
 btmMid = (int(width/2),int(2*height/3))
 btmLt = (int(3*width/4),int(2*height/3))
-cirSize = redSize = greenSize = blueSize = yellowSize = pinkSize = cyanSize = 10
+
+#initial circle size
+cirSize = redSize = greenSize = blueSize = yellowSize = pinkSize = cyanSize = 10 
 
 screen = pygame.display.set_mode(size)
 screen.fill(black)
 
+#first circles
 redCir = pygame.draw.circle(screen,red,topRt,cirSize)
 greenCir = pygame.draw.circle(screen,green,topMid,cirSize)
 blueCir = pygame.draw.circle(screen,blue,topLt,cirSize)
@@ -36,37 +43,30 @@ yellowCir = pygame.draw.circle(screen,yellow,btmRt,cirSize)
 pinkCir = pygame.draw.circle(screen,pink,btmMid,cirSize)
 cyanCir = pygame.draw.circle(screen,cyan,btmLt,cirSize)
 
-redSize+=random.randrange(int((-1*cirSize)+10),int(2*cirSize/3))
-greenSize+=random.randrange(int((-1*cirSize)+10),int(2*cirSize/3))
-blueSize+=random.randrange(int((-1*cirSize)+10),int(2*cirSize/3))
-yellowSize+=random.randrange(int((-1*cirSize)+10),int(2*cirSize/3))
-pinkSize+=random.randrange(int((-1*cirSize)+10),int(2*cirSize/3))
-cyanSize+=random.randrange(int((-1*cirSize)+10),int(2*cirSize/3))
-
 while 1:
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT: sys.exit()
 	
 	screen.fill(black)
-	for x in range(1,7):
-		if x==1:
-			redCir = pygame.draw.circle(screen,red,topRt,redSize)
-			redSize+=random.randrange(int((-1*redSize)+10),int(2*redSize/3))
-		elif x==2:
-			greenCir = pygame.draw.circle(screen,green,topMid,greenSize)
-			greenSize+=random.randrange(int((-1*greenSize)+10),int(2*greenSize/3))
-		elif x==3:
-			blueCir = pygame.draw.circle(screen,blue,topLt,blueSize)
-			blueSize+=random.randrange(int((-1*blueSize)+10),int(2*blueSize/3))
-		elif x==4:
-			yellowCir = pygame.draw.circle(screen,yellow,btmRt,yellowSize)
-			yellowSize+=random.randrange(int((-1*yellowSize)+10),int(2*yellowSize/3))
-		elif x==5:
-			pinkCir = pygame.draw.circle(screen,pink,btmMid,pinkSize)
-			pinkSize+=random.randrange(int((-1*pinkSize)+10),int(2*pinkSize/3))
-		else:
-			cyanCir = pygame.draw.circle(screen,cyan,btmLt,cyanSize)
-			cyanSize+=random.randrange(int((-1*cyanSize)+10),int(2*cyanSize/3))
+	
+	#change circle sizes
+	redCir = pygame.draw.circle(screen,red,topRt,redSize)
+	redSize+=random.randrange(int((-1*redSize)+10),int(2*redSize/3))
+
+	greenCir = pygame.draw.circle(screen,green,topMid,greenSize)
+	greenSize+=random.randrange(int((-1*greenSize)+10),int(2*greenSize/3))
+
+	blueCir = pygame.draw.circle(screen,blue,topLt,blueSize)
+	blueSize+=random.randrange(int((-1*blueSize)+10),int(2*blueSize/3))
+
+	yellowCir = pygame.draw.circle(screen,yellow,btmRt,yellowSize)
+	yellowSize+=random.randrange(int((-1*yellowSize)+10),int(2*yellowSize/3))
+
+	pinkCir = pygame.draw.circle(screen,pink,btmMid,pinkSize)
+	pinkSize+=random.randrange(int((-1*pinkSize)+10),int(2*pinkSize/3))
+
+	cyanCir = pygame.draw.circle(screen,cyan,btmLt,cyanSize)
+	cyanSize+=random.randrange(int((-1*cyanSize)+10),int(2*cyanSize/3))
 	
 	pygame.time.wait(300)
 	pygame.display.update()
