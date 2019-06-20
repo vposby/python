37e6eeb4d5ff75c0feac27f10d8e731b505eb99b
vposby <50159345+vposby@users.noreply.github.com>
2019-06-20 15:43:11 -0700

    Add files via upload

--- ers.py
+++ ers.py
@@ -51,13 +51,13 @@ while invalid < 4:
 		while hChosen == False:
 			human = str(input('How many human players will there be? '))
 			if human.isdigit() == False:
-				invalid+=1
-				print('Invalid entry! You have ' + str(3-invalid) + ' attempts before the program closes.')
+				print('Invalid entry! You have ' + str(3-invalid) + ' attempt(s) before the program closes.')
 				print('Please enter a digit between 1 and 6.')
+				invalid+=1
 			elif int(human) < 1 or int(human) > 6:
-				invalid+=1
-				print('Invalid entry! You have ' + str(3-invalid) + ' attempts before the program closes.')
+				print('Invalid entry! You have ' + str(3-invalid) + ' attempt(s) before the program closes.')
 				print('Please enter a digit between 1 and 6.')
+				invalid+=1
 			else:
 				human = int(human)
 				if human == 6:
@@ -71,40 +71,40 @@ while invalid < 4:
 			computer = str(input('How many computer players will there be? '))
 			cLevel = 0
 			if computer.isdigit() == False:
+				print('Invalid entry! You have ' + str(3-invalid) + ' attempt(s) before the program closes.')
+				print('Please enter a digit between 0 and ' + str(6-human) + '.')
 				invalid+=1
-				print('Invalid entry! You have ' + str(3-invalid) + ' attempts before the program closes.')
-				print('Please enter a digit between 1 and ' + str(6-human) + '.')
 			elif int(computer) < 0 or int(computer) > 6-human:
+				print('Invalid entry! You have ' + str(3-invalid) + ' attempt(s) before the program closes.')
+				print('Please enter a digit between 0 and ' + str(6-human) + '.')
 				invalid+=1
-				print('Invalid entry! You have ' + str(3-invalid) + ' attempts before the program closes.')
-				print('Please enter a digit between 1 and ' + str(6-human) + '.')
 			elif human + int(computer) == 1:
-				invalid+=1
-				print('Invalid entry! You have ' + str(3-invalid) + ' attempts before the program closes.')
+				print('Invalid entry! You have ' + str(3-invalid) + ' attempt(s) before the program closes.')
 				print('Please enter a digit between 1 and ' + str(6-human) + '.')
+				invalid+=1
 			else:
 				players = human + int(computer)
 				computer = int(computer)
 
-				#choose computer player difficulty
+				#choose computer player difficulty (slap speed in seconds)
 				if computer != 0:
 					cLevel = str(input('Choose computer player difficulty (1=Easy, 2=Medium, 3=Hard): '))
 					if cLevel.isdigit() == False:
-						invalid+=1
-						print('Invalid entry! You have ' + str(3-invalid) + ' attempts before the program closes.')
+						print('Invalid entry! You have ' + str(3-invalid) + ' attempt(s) before the program closes.')
 						print('Please enter a digit between 1 and 3.')
+						invalid+=1
 					elif int(cLevel) < 1 or int(cLevel) > 3:
-						invalid+=1
-						print('Invalid entry! You have ' + str(3-invalid) + ' attempts before the program closes.')
+						print('Invalid entry! You have ' + str(3-invalid) + ' attempt(s) before the program closes.')
 						print('Please enter a digit between 1 and 3.')
+						invalid+=1
 					else:
-						cLevel = [int(cLevel),'']
+						cLevel = [int(cLevel),''] #do this for each computer player later
 						if cLevel[0] == 1:
-							cLevel[1] = 'Easy'
+							cLevel[1] = 'Easy' #2 seconds
 						elif cLevel[0] == 2:
-							cLevel[1] = 'Medium'
+							cLevel[1] = 'Medium' #1.5 seconds
 						else:
-							cLevel[1] = 'Hard'
+							cLevel[1] = 'Hard' #1 second
 				cChosen = True
 	
 	print('Human Players: ' + str(human))
@@ -126,33 +126,65 @@ while invalid < 4:
 	person = 0
 
 	for card in deck:
-		card[2]=player[person]
-		card[3]=order
+		card[2] = player[person][0]
+		card[3] = order
 		player[person][2]+=1
 		dealt+=1
-		if dealt%(len(player)+1) == 0:
-			order+=1
 		if person+1 < len(player):
 			person+=1
 		else:
 			person = 0
-
-	print(player)
+			order+=1
 	
 	#randomize starting player
 	starter = random.randrange(0,len(player))
 	print(player[starter][0] + ' will start!')
+	print(player)
+
+	#rearrange player order
+	for x in range(starter):
+		player.append(player[0])
+		player.pop(0)
+		player[len(player)-1][1] = x
 
 	#begin play
 	gameOver = False
 	handOver = False
 
-	
-	#if number, go to next player
+	while gameOver == False:
+		order = 1
+		while handOver == False:
+			play = 1
+			for x in range(len(player)):
+				for card in deck:
+					if card[2] == player[x][0] and card[3] == order:
+						card[2] = 'in play'
+						card[3] = play
+						play+=1
+						if card[0] == 'Jack':
+							if x+1>len(player):
 
+							else:
+
+						elif card[0] == 'Queen':
+							if x+1>len(player):
+							else:
+
+						elif card[0] == 'King':
+							if x+1>len(player):
+							else:
+
+						elif card[0] == 'Ace':
+							if x+1>len(player):
+							else:
+
+						else:
+
+	#if number, go to next player
 	#if face card, next player puts down required cards
 	#if required cards max number reached, face card player collects
 	#if one player has all 52 cards, game ends
 	#player x wins message
-	print('Goodbye!')
-	sys.exit()
+print('Goodbye!')
+sys.exit()
+
